5942c31af3a0de3ce8fa84a0784ee7ad73e289cd
Ayushman Chakravarty <ayushmanchak@gmail.com>
2020-09-16 20:39:47 +0530

    more readable :D

--- dynamic_array.java
+++ dynamic_array.java
@@ -9,114 +9,124 @@ public class DynamicArray<T> implements Iterable<T> {
     this(16);
   }
 
-  public DynamicArray(int capacity) {
-    if (capacity < 0) throw new IllegalArgumentException("Illegal Capacity: " + capacity);
-    this.capacity = capacity;
-    arr = (T[]) new Object[capacity];
+  public DynamicArray(final int capacity) {
+      if (capacity < 0)
+          throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+      this.capacity = capacity;
+      arr = (T[]) new Object[capacity];
   }
 
   public int size() {
-    return len;
+      return len;
   }
 
   public boolean isEmpty() {
-    return size() == 0;
+      return size() == 0;
   }
 
-  public T get(int index) {
-    return arr[index];
+  public T get(final int index) {
+      return arr[index];
   }
 
-  public void set(int index, T elem) {
-    arr[index] = elem;
+  public void set(final int index, final T elem) {
+      arr[index] = elem;
   }
 
   public void clear() {
-    for (int i = 0; i < len; i++) arr[i] = null;
-    len = 0;
+      for (int i = 0; i < len; i++)
+          arr[i] = null;
+      len = 0;
   }
 
-  public void add(T elem) {
+  public void add(final T elem) {
 
-    // Time to resize!
-    if (len + 1 >= capacity) {
-      if (capacity == 0) capacity = 1;
-      else capacity *= 2; // double the size
-      T[] new_arr = (T[]) new Object[capacity];
-      for (int i = 0; i < len; i++) new_arr[i] = arr[i];
-      arr = new_arr; // arr has extra nulls padded
-    }
+      // Time to resize!
+      if (len + 1 >= capacity) {
+          if (capacity == 0)
+              capacity = 1;
+          else
+              capacity *= 2; // double the size
+          final T[] new_arr = (T[]) new Object[capacity];
+          for (int i = 0; i < len; i++)
+              new_arr[i] = arr[i];
+          arr = new_arr; // arr has extra nulls padded
+      }
 
-    arr[len++] = elem;
+      arr[len++] = elem;
   }
 
   // Removes an element at the specified index in this array.
-  public T removeAt(int rm_index) {
-    if (rm_index >= len || rm_index < 0) throw new IndexOutOfBoundsException();
-    T data = arr[rm_index];
-    T[] new_arr = (T[]) new Object[len - 1];
-    for (int i = 0, j = 0; i < len; i++, j++)
-      if (i == rm_index) j--; // Skip over rm_index by fixing j temporarily
-      else new_arr[j] = arr[i];
-    arr = new_arr;
-    capacity = --len;
-    return data;
+  public T removeAt(final int rm_index) {
+      if (rm_index >= len || rm_index < 0)
+          throw new IndexOutOfBoundsException();
+      final T data = arr[rm_index];
+      final T[] new_arr = (T[]) new Object[len - 1];
+      for (int i = 0, j = 0; i < len; i++, j++)
+          if (i == rm_index)
+              j--; // Skip over rm_index by fixing j temporarily
+          else
+              new_arr[j] = arr[i];
+      arr = new_arr;
+      capacity = --len;
+      return data;
   }
 
-  public boolean remove(Object obj) {
-    int index = indexOf(obj);
-    if (index == -1) return false;
-    removeAt(index);
-    return true;
+  public boolean remove(final Object obj) {
+      final int index = indexOf(obj);
+      if (index == -1)
+          return false;
+      removeAt(index);
+      return true;
   }
 
-  public int indexOf(Object obj) {
-    for (int i = 0; i < len; i++) {
-      if (obj == null) {
-        if (arr[i] == null) return i;
-      } else {
-        if (obj.equals(arr[i])) return i;
+  public int indexOf(final Object obj) {
+      for (int i = 0; i < len; i++) {
+          if (obj == null) {
+              if (arr[i] == null)
+                  return i;
+          } else {
+              if (obj.equals(arr[i]))
+                  return i;
+          }
       }
-    }
-    return -1;
+      return -1;
   }
 
-  public boolean contains(Object obj) {
-    return indexOf(obj) != -1;
+  public boolean contains(final Object obj) {
+      return indexOf(obj) != -1;
   }
 
   // Iterator is still fast but not as fast as iterative for loop
   @Override
   public java.util.Iterator<T> iterator() {
-    return new java.util.Iterator<T>() {
-      int index = 0;
+      return new java.util.Iterator<T>() {
+          int index = 0;
 
-      @Override
-      public boolean hasNext() {
-        return index < len;
-      }
+          @Override
+          public boolean hasNext() {
+              return index < len;
+          }
 
-      @Override
-      public T next() {
-        return arr[index++];
-      }
+          @Override
+          public T next() {
+              return arr[index++];
+          }
 
-      @Override
-      public void remove() {
-        throw new UnsupportedOperationException();
-      }
-    };
+          @Override
+          public void remove() {
+              throw new UnsupportedOperationException();
+          }
+      };
   }
 
   @Override
   public String toString() {
-    if (len == 0) return "[]";
-    else {
-      StringBuilder sb = new StringBuilder(len).append("[");
+      if (len == 0)
+          return "[]";
+      else {
+          final StringBuilder sb = new StringBuilder(len).append("[");
       for (int i = 0; i < len - 1; i++) sb.append(arr[i] + ", ");
       return sb.append(arr[len - 1] + "]").toString();
     }
   }
 }
-
-//Now your Dynamic array class is all set to be used!
